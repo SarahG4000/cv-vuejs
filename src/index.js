a208be2c7f3ed43995d534e5907330d3f98c1046
SarahG4000 <sarah.guillaume4000@gmail.com>
2020-07-16 13:36:24 +0200

    hello world hello parcel

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+ * creation of the file contenaier
+ * src/index.js - container for app
+ * start created 14/07/2020
+ */
+
+// import Vue from "vue";
+import App from "./components/app.vue";
+
+new Vue({render: createElement => createElement(App)}).$mount("#appli");
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello world!!",
+    },
+});
+
+console.log("hello word:!!");
